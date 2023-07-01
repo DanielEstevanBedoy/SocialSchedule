e1e5726ac62adf01d0504d6ccc1364938360e7f7
Estevan Bedoy <destevanbedoy@gmail.com>
2023-07-01 15:23:31 -0700

    Used MaterialUI to create a SignIn sheet and began the sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Dashboard from './pages/dashboard'
+import SignInSide from './pages/SignIn';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/DashBoard" element = {<Dashboard/>}/>
+        <Route path = "/SignIn" element = {<SignInSide/>} />
+      </Routes>
+      </BrowserRouter>
+
+      
+
       
     </div>
   );
---
+++ src/pages/dashboard.js
@@ -0,0 +1,12 @@
+import React from'react'
+
+export default function Dashboard(){
+    
+    return(
+    <>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    </>
+    );
+}
